0fe7290b586478061ff33cd3cc8e14525983df0e
guimassoqueto <gmassoqueto@gmail.com.br>
2023-03-29 17:09:03 -0300

    feat: ensure SignUpControlller returns 400 if no name is provided

---
+++ tests/signup.test.ts
@@ -0,0 +1,19 @@
+import { SignUpControlller } from "../src/presentation/controllers/signup.controller"
+
+describe('Sign Up Controlller' , () => {
+  test('Should return 400 if no name is provided', () => {
+    const sut = new SignUpControlller();
+
+    const httpRequest = {
+      body: {
+        name: "any_name",
+        email: "any_email@email.com",
+        password: "any_password",
+        passwordConfirmation: "any_password"
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+  })
+})
