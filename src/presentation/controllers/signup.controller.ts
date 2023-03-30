41e419a5f35cd9d49764f44904cbfd32a5101c25
guimassoqueto <gmassoqueto@gmail.com.br>
2023-03-30 16:02:02 -0300

    feat: enrure SignUpController returns error if an invalid email is provided

--- src/presentation/controllers/signup.controller.ts
+++ src/presentation/controllers/signup.controller.ts
@@ -2,8 +2,16 @@ import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
 import { type Controller } from '../protocols/controller'
 import { type IHttpRequest, type IHttpResponse } from '../protocols/http'
+import { type EmailValidator } from '../protocols/email-validator'
+import { InvalidParamError } from '../errors/invalid-param-error'
 
 export class SignUpControlller implements Controller {
+  private readonly emailValidator: EmailValidator
+
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   public handle (httpRequest: IHttpRequest): IHttpResponse {
     const requiredFields: string[] = ['name', 'email', 'password', 'passwordConfirmation']
 
@@ -12,6 +20,10 @@ export class SignUpControlller implements Controller {
     for (const field of requiredFields) {
       if (!(field in httpRequest.body)) return badRequest(new MissingParamError(field))
     }
+
+    const isValidEmail = this.emailValidator.isValid(httpRequest.body.email)
+
+    if (!isValidEmail) return badRequest(new InvalidParamError('email'))
 
     return {
       statusCode: 200,
