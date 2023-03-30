d085598649bfc12e1ba0a8577a224c43fab0dd64
guimassoqueto <gmassoqueto@gmail.com.br>
2023-03-30 08:40:16 -0300

    test: ensure SignUpController return 400 if passwordConfirmation is not provided

--- tests/signup.test.ts
+++ tests/signup.test.ts
@@ -51,4 +51,20 @@ describe('Sign Up Controlller' , () => {
     expect(httpResponse.body).toEqual(new MissingParamError("password"));
   })
 
+  test('Should return 400 if no passwordConfirmation is provided', () => { 
+    const sut = new SignUpControlller();
+
+    const httpRequest: IHttpRequest = {
+      body: {
+        name: "any_name",
+        email: "any_email@email.com",
+        password: "any_password"
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError("passwordConfirmation"));
+  })
+
 })
