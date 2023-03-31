4152f8248716509717cf5b55be31c5b25ebd77ad
guimassoqueto <gmassoqueto@gmail.com.br>
2023-03-31 04:49:51 -0300

    feat: ensure SignUpController returns 500 if EmailValidator throws

--- src/presentation/controllers/signup.controller.ts
+++ src/presentation/controllers/signup.controller.ts
@@ -5,6 +5,7 @@ import { type Controller } from '../protocols/controller'
 import { type IHttpRequest, type IHttpResponse } from '../protocols/http'
 import { type EmailValidator } from '../protocols/email-validator'
 import { type PasswordValidator } from '../protocols/password-validator'
+import { ServerError } from '../errors/server-error'
 
 export class SignUpControlller implements Controller {
   private readonly emailValidator: EmailValidator
@@ -16,23 +17,30 @@ export class SignUpControlller implements Controller {
   }
 
   public handle (httpRequest: IHttpRequest): IHttpResponse {
-    const requiredFields: string[] = ['name', 'email', 'password', 'passwordConfirmation']
+    try {
+      const requiredFields: string[] = ['name', 'email', 'password', 'passwordConfirmation']
 
-    // checa se os campos obrigatórios esperados estão presentes no corpo da requisição
+      // checa se os campos obrigatórios esperados estão presentes no corpo da requisição
 
-    for (const field of requiredFields) {
-      if (!(field in httpRequest.body)) return badRequest(new MissingParamError(field))
-    }
+      for (const field of requiredFields) {
+        if (!(field in httpRequest.body)) return badRequest(new MissingParamError(field))
+      }
 
-    const isValidEmail = this.emailValidator.isValid(httpRequest.body.email)
-    if (!isValidEmail) return badRequest(new InvalidParamError('email'))
+      const isValidEmail = this.emailValidator.isValid(httpRequest.body.email)
+      if (!isValidEmail) return badRequest(new InvalidParamError('email'))
 
-    const isValidPassword = this.passwordValidator.isStrong(httpRequest.body.password)
-    if (!isValidPassword) return badRequest(new InvalidParamError('password'))
+      const isValidPassword = this.passwordValidator.isStrong(httpRequest.body.password)
+      if (!isValidPassword) return badRequest(new InvalidParamError('password'))
 
-    return {
-      statusCode: 200,
-      body: 'Success'
+      return {
+        statusCode: 200,
+        body: 'Success'
+      }
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: new ServerError()
+      }
     }
   }
 }
