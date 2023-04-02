753077fc0bb0ad323a4aa7e16e63773c1d627b53
guimassoqueto <gui.massoqueto@gmail.com>
2023-04-02 05:50:32 -0300

    chore: update the Controller definition

--- src/presentation/protocols/controller.ts
+++ src/presentation/protocols/controller.ts
@@ -1,5 +1,5 @@
-import { type IHttpRequest, type IHttpResponse } from './http'
+import { type HttpRequest, type HttpResponse } from './http'
 
 export interface Controller {
-  handle: (httpRequest: IHttpRequest) => IHttpResponse
+  handle: (httpRequest: HttpRequest) => Promise<HttpResponse>
 }
