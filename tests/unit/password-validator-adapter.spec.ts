afd31f8de15f3d0edc33e32eaf95b58c70c412a9
guimassoqueto <gui.massoqueto@gmail.com>
2023-04-03 07:25:06 -0300

    test: ensures isStrong receives as argument the same data provided by the user

--- tests/unit/password-validator-adapter.spec.ts
+++ tests/unit/password-validator-adapter.spec.ts
@@ -11,13 +11,22 @@ describe('PasswordValidatorAdapter', () => {
   test('Should return false if the provided password is weak', async () => {
     const sut = makeSut();
     const isStrongPassword = await sut.isStrong("password123");
-    expect(isStrongPassword).toBe(false);
+    expect(isStrongPassword).toBe(false)
   })
 
   test('Should return true if the provided password is strong', async () => {
     const sut = makeSut();
     const isStrongPassword = await sut.isStrong("#!@1QWrt34 !@");
-    expect(isStrongPassword).toBe(true);
+    expect(isStrongPassword).toBe(true)
   })
 
+  test('Should return true if the provided password by the user is the same as used by isStrong', async () => {
+    const sut = makeSut();
+    const spiedMethod = jest.spyOn(sut, "isStrong")
+
+    const correctPassword = "some_password"
+    await sut.isStrong(correctPassword)
+
+    expect(spiedMethod).toHaveBeenCalledWith(correctPassword)
+  })
 })
