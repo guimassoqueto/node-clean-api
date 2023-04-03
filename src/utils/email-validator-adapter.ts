36393a8c084466238eabbdc67433f61472038916
guimassoqueto <gui.massoqueto@gmail.com>
2023-04-03 06:24:53 -0300

    refactor: simplify isValid method

--- src/utils/email-validator-adapter.ts
+++ src/utils/email-validator-adapter.ts
@@ -3,14 +3,7 @@ import validator from 'validator'
 
 export class EmailValidatorAdapter implements EmailValidator {
   async isValid (email: string): Promise<boolean> {
-    try {
-      const value = validator.isEmail(email)
-      return await new Promise((resolve, reject) => {
-        resolve(value)
-      })
-    } catch (error) {
-      console.error(error)
-      throw error
-    }
+    const value = validator.isEmail(email)
+    return await new Promise(resolve => { resolve(value) })
   }
 }
