480b6e0c5390aa4444b1d6e55fddf6d56f139eba
guimassoqueto <gui.massoqueto@gmail.com>
2023-04-03 05:55:11 -0300

    test: validate email in true or false cases

--- tests/unit/email-validator-adapter.spec.ts
+++ tests/unit/email-validator-adapter.spec.ts
@@ -2,13 +2,12 @@ import { EmailValidatorAdapter } from "../../src/utils/email-validator-adapter";
 
 describe('Email Validator Adapter' , () => {
   test('should return false if the email passed is invalid', async () => {
-    const sut = new EmailValidatorAdapter()
-
+    const sut = new EmailValidatorAdapter();
     const isValid = await sut.isValid('invalid_email.com');
     expect(isValid).toBe(false);
   })
 
-  test('should return false if the email passed is valid', async () => {
+  test('should return true if the email passed is valid', async () => {
     const sut = new EmailValidatorAdapter()
 
     const isValid = await sut.isValid('valid_email@gmail.com');
