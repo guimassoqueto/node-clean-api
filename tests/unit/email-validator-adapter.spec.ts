e74555bca1a7984a6217bbd83361e7a2f00b7d7d
guimassoqueto <gui.massoqueto@gmail.com>
2023-04-03 06:29:09 -0300

    refactor: move sut creation to a Factory method

--- tests/unit/email-validator-adapter.spec.ts
+++ tests/unit/email-validator-adapter.spec.ts
@@ -1,35 +1,40 @@
 import { EmailValidatorAdapter } from "../../src/utils/email-validator-adapter";
-import validator from "validator";
+
+// Factory que cria um EmailValidator
+function makeSut(): EmailValidatorAdapter {
+  const emailValidatorStub = new EmailValidatorAdapter()
+  return emailValidatorStub;
+}
 
 describe('Email Validator Adapter' , () => {
   test('should return false if the email passed is invalid', async () => {
-    const sut = new EmailValidatorAdapter();
+    const sut = makeSut();
     const isValid = await sut.isValid('invalid_email.com');
     expect(isValid).toBe(false);
   })
 
   test('should return true if the email passed is valid', async () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
 
-    const isValid = await sut.isValid('valid_email@gmail.com');
-    expect(isValid).toBe(true);
+    const isValid = await sut.isValid('valid_email@gmail.com')
+    expect(isValid).toBe(true)
   })
 
   test('should return false if the email passed is an empty string', async () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
 
     const isValid = await sut.isValid('');
-    expect(isValid).toBe(false);
+    expect(isValid).toBe(false)
   })
 
   test('Should call validator with correct email', async () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     const isEmailSpy = jest.spyOn(sut, "isValid")
 
     // corfirma que o email passado no método é o mesmo email usado na verificação
     const correct_email = "correct_email@gmail.com"
     await sut.isValid(correct_email)
 
-    expect(isEmailSpy).toHaveBeenCalledWith(correct_email);
+    expect(isEmailSpy).toHaveBeenCalledWith(correct_email)
   })
 })
