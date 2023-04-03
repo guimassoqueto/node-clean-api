545d5fcaa3fefb6e6a0b23233520b3e0a68e0e89
guimassoqueto <gui.massoqueto@gmail.com>
2023-04-03 05:53:52 -0300

    feat: add validator to isValid method

--- src/utils/email-validator-adapter.ts
+++ src/utils/email-validator-adapter.ts
@@ -1,10 +1,16 @@
 import { type EmailValidator } from '../presentation/protocols'
+import validator from 'validator'
 
 export class EmailValidatorAdapter implements EmailValidator {
   async isValid (email: string): Promise<boolean> {
-    return await new Promise((resolve, reject) => {
-      if (!email.includes('@')) resolve(false)
-      else resolve(true)
-    })
+    try {
+      const value = validator.isEmail(email)
+      return await new Promise((resolve, reject) => {
+        resolve(value)
+      })
+    } catch (error) {
+      console.error(error)
+      throw error
+    }
   }
 }
