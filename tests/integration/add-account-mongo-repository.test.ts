e770f44a4d516055923afdbbc4d9790fc0a0cfcf
guimassoqueto <gui.massoqueto@gmail.com>
2023-04-04 17:31:06 -0300

    refactor: clean the mongo database collection after each test execution

--- tests/integration/add-account-mongo-repository.test.ts
+++ tests/integration/add-account-mongo-repository.test.ts
@@ -15,20 +15,25 @@ describe('Add Account Mongo Repository' , () => {
     await MongoHelper.disconnect();
   })
 
+  afterEach(async () => {
+    const accountCollection = MongoHelper.getCollection("accounts")
+    await accountCollection.deleteMany({})
+  })
+
   test('Should return an account on success', async () => {
     const sut = makeSut()
 
     const account = await sut.add({
-      name: "any_name",
-      email: "any_email",
-      password: "any_password"
+      name: "bibaman",
+      email: "bibaman@email.com",
+      password: "bibamanPassword"
     })
 
     expect(account).toBeTruthy()
     expect(account.id).toBeTruthy()
-    expect(account.name).toBe("any_name")
-    expect(account.email).toBe("any_email")
-    expect(account.password).toBe("any_password")
+    expect(account.name).toBe("bibaman")
+    expect(account.email).toBe("bibaman@email.com")
+    expect(account.password).toBe("bibamanPassword")
   })
 
 })
