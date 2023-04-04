aa598170fcb152db1fb38eeaf7a3b4177c01f5e6
guimassoqueto <gui.massoqueto@gmail.com>
2023-04-04 17:13:29 -0300

    test: feat: ensure AddAccountMongoRepository return an account on insertion success

--- tests/integration/basic.test.ts
+++ tests/integration/basic.test.ts
@@ -1,5 +1,30 @@
-describe('simple' , () => {
-  test('ok', () => {
-    expect(1+1).toBe(2);
+import { MONGO_URL } from "../../src/settings"
+import { MongoHelper } from "../../src/infra/db/mongodb/helpers/mongo-helper"
+import { AddAccountMongoRepository } from "../../src/infra/db/mongodb/account-repository/account"
+
+describe('Account Mongo Repository' , () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(MONGO_URL);
   })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  })
+
+  test('Should return an account on success', async () => {
+    const sut = new AddAccountMongoRepository()
+
+    const account = await sut.add({
+      name: "any_name",
+      email: "any_email",
+      password: "any_password"
+    })
+
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe("any_name")
+    expect(account.email).toBe("any_email")
+    expect(account.password).toBe("any_password")
+  })
+
 })
