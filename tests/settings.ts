a9d3070e555058d887990984d234f41031c9635e
guimassoqueto <gui.massoqueto@gmail.com>
2023-04-04 13:30:20 -0300

    test: add settings file with variables to access the mongo container

---
+++ tests/settings.ts
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+const MONGO_ROOT_USERNAME = process.env.MONGO_ROOT_USERNAME || "username";
+const MONGO_ROOT_PASSWORD = process.env.MONGO_ROOT_PASSWORD || "password";
+// const MONGO_DB_NAME = process.env.MONGO_DB_NAME || "test";
+const MONGO_HOST = process.env.MONGO_HOST || "0.0.0.0";
+const MONGO_PORT = process.env.MONGO_PORT || "27017";
+
+//export const MONGO_URL = `mongodb://${MONGO_ROOT_USERNAME}:${MONGO_ROOT_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB_NAME}?authSource=admin`;
+export const MONGO_URL = `mongodb://${MONGO_ROOT_USERNAME}:${MONGO_ROOT_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}?authSource=admin`;
