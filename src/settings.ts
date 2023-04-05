b8da98594c612c3aec61191ca0184efecd49e436
guimassoqueto <gui.massoqueto@gmail.com>
2023-04-05 06:01:27 -0300

    feat: update .env.example and add the new variable to settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -7,4 +7,6 @@ export const MONGO_DB_NAME = process.env.MONGO_DB_NAME ?? 'test'
 const MONGO_HOST = process.env.MONGO_HOST ?? '0.0.0.0'
 const MONGO_PORT = process.env.MONGO_PORT ?? '27017'
 
+export const APP_PORT = parseInt(process.env.APP_PORT ?? '8000')
+
 export const MONGO_URL = `mongodb://${MONGO_ROOT_USERNAME}:${MONGO_ROOT_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}?authSource=admin`
