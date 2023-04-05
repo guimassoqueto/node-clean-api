62ce8b8bb612f9994c9c45a8744edef6f8c913ca
guimassoqueto <gui.massoqueto@gmail.com>
2023-04-05 06:21:49 -0300

    refactor: move main files and change  npm command to reflect these changes

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,4 +1,4 @@
-import { app } from './main/server'
-import { APP_PORT } from './settings'
+import { app } from './config/app'
+import { APP_PORT } from '../settings'
 
 app.listen(APP_PORT, () => { console.log(`server is listening to ${APP_PORT}`) })
