643aea20deeb48606d4e21144cbc500a05e22302
guimassoqueto <gui.massoqueto@gmail.com>
2023-04-09 11:12:27 -0300

    refactor: change the return type to the makeSut factory method

--- tests/unit/controller.login-controller.spec.ts
+++ tests/unit/controller.login-controller.spec.ts
@@ -1,11 +1,18 @@
 import { LoginController } from "../../src/presentation/controllers/login/login-controller"
 import { HttpRequest } from "../../src/presentation/controllers/login/login-protocols"
-import { badRequest, ok } from "../../src/presentation/helpers/http-helper"
+import { badRequest } from "../../src/presentation/helpers/http-helper"
 import { MissingParamError  } from "../../src/presentation/errors"
 
+interface SutTypes {
+  sut: LoginController
+}
 
-function makeSut(): LoginController {
-  return new LoginController()
+function makeSut(): SutTypes {
+  const sut = new LoginController()
+
+  return {
+    sut
+  }
 }
 
 function makeFakeHttpRequest(): HttpRequest {
@@ -19,7 +26,7 @@ function makeFakeHttpRequest(): HttpRequest {
 
 describe('Login Controller' , () => {
   test('Should return 400 if no email is provided', async () => {
-    const sut = makeSut();
+    const { sut } = makeSut();
     const httpRequest = makeFakeHttpRequest()
     delete httpRequest.body.email;
     const httpResponse = await sut.handle(httpRequest)
@@ -28,7 +35,7 @@ describe('Login Controller' , () => {
   })
 
   test('Should return 400 if no password is provided', async () => {
-    const sut = makeSut();
+    const { sut } = makeSut();
     const httpRequest = makeFakeHttpRequest()
     delete httpRequest.body.password;
     const httpResponse = await sut.handle(httpRequest)
