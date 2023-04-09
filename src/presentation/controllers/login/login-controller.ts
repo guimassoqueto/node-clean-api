b76a1f1f6f07b20ae4a0af9a554d33f92525b3c5
guimassoqueto <gui.massoqueto@gmail.com>
2023-04-09 11:04:13 -0300

    feat: ensures return 400 whether email or password is not provided

--- src/presentation/controllers/login/login-controller.ts
+++ src/presentation/controllers/login/login-controller.ts
@@ -1,11 +1,20 @@
 import { type Controller, type HttpRequest, type HttpResponse } from './login-protocols'
-import { serverError, badRequest } from '../../helpers/http-helper'
+import { serverError, badRequest, ok } from '../../helpers/http-helper'
 import { MissingParamError } from '../../errors'
 
 export class LoginController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      return await new Promise(resolve => { resolve(badRequest(new MissingParamError('email'))) })
+      const requiredFields: string[] = ['email', 'password']
+
+      // checa se os campos obrigatórios esperados estão presentes no corpo da requisição
+      for (const field of requiredFields) {
+        if (!(field in httpRequest.body)) {
+          return await new Promise(resolve => { resolve(badRequest(new MissingParamError(field))) })
+        }
+      }
+
+      return ok('success')
     } catch (error) {
       return serverError(error.stack)
     }
