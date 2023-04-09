206d779149d1224fc0e9d76eaf8121c6bd59c3ab
guimassoqueto <gui.massoqueto@gmail.com>
2023-04-09 11:36:00 -0300

    feat: ensures return 400 if the email provided in the request body is invalid

--- src/presentation/controllers/login/login-controller.ts
+++ src/presentation/controllers/login/login-controller.ts
@@ -1,8 +1,12 @@
-import { type Controller, type HttpRequest, type HttpResponse } from './login-protocols'
+import { type EmailValidator, type Controller, type HttpRequest, type HttpResponse } from './login-protocols'
 import { serverError, badRequest, ok } from '../../helpers/http-helper'
-import { MissingParamError } from '../../errors'
+import { InvalidParamError, MissingParamError } from '../../errors'
 
 export class LoginController implements Controller {
+  constructor (
+    private readonly emailValidator: EmailValidator
+  ) {}
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const requiredFields: string[] = ['email', 'password']
@@ -10,9 +14,15 @@ export class LoginController implements Controller {
       // checa se os campos obrigatórios esperados estão presentes no corpo da requisição
       for (const field of requiredFields) {
         if (!(field in httpRequest.body)) {
-          return await new Promise(resolve => { resolve(badRequest(new MissingParamError(field))) })
+          return badRequest(new MissingParamError(field))
         }
       }
+
+      const { email } = httpRequest.body
+
+      // valida o email do usuário
+      const isValidEmail = await this.emailValidator.isValid(email)
+      if (!isValidEmail) return badRequest(new InvalidParamError('email'))
 
       return ok('success')
     } catch (error) {
