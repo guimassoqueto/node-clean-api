e7c7c75b9dcfee8b5f4a76418719326b6850c3f3
guimassoqueto <gui.massoqueto@gmail.com>
2023-04-11 18:42:39 -0300

    chore: remove useless import

--- src/presentation/controllers/signup/signup-controller.ts
+++ src/presentation/controllers/signup/signup-controller.ts
@@ -6,7 +6,6 @@ import {
   type Validation
 } from './signup-protocols'
 import { badRequest, ok, serverError } from '../../helpers/http-helper'
-import { InvalidParamError } from '../../errors'
 
 export class SignUpControlller implements Controller {
   constructor (
@@ -18,15 +17,8 @@ export class SignUpControlller implements Controller {
     try {
       const error = this.validation.validate(httpRequest.body)
       if (error) return badRequest(error)
-
       const { name, email, password } = httpRequest.body
-
-      // TODO: remover linha abaixo
-      // valida que o nome existe e não é vazio e é composto por no mínimo 3 letras
-      if ((name as string).length < 3) return badRequest(new InvalidParamError('name'))
-
       const account = await this.addAccount.add({ name, email, password })
-
       return ok(account)
     } catch (error) {
       return serverError(error.stack)
