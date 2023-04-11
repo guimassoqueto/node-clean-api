1ee11d0189605619933e0c924f88bb8d1231b136
guimassoqueto <gui.massoqueto@gmail.com>
2023-04-11 08:08:42 -0300

    refactor: add factory function to create EmailValidator

--- tests/unit/factory.signup-validation-factory.test.ts
+++ tests/unit/factory.signup-validation-factory.test.ts
@@ -2,11 +2,22 @@ import { makeSignUpValidation } from "../../src/main/factories/signup-validation
 import { ValidationComposite } from "../../src/presentation/helpers/validators/validation-composite"
 import { 
   RequiredFieldValidation, 
-  FieldsComparisonValidation 
+  FieldsComparisonValidation,
+  EmailValidation
 } from "../../src/presentation/helpers/validators/validations/"
 import { Validation } from "../../src/presentation/helpers/validators/validation"
+import { EmailValidator } from "../../src/presentation/protocols"
 
 jest.mock("../../src/presentation/helpers/validators/validation-composite")
+
+function makeEmailValidator(): EmailValidator {
+  class EmailValidatorStub implements EmailValidator {
+    isValid (email: string) {
+      return true
+    }
+  }
+  return new EmailValidatorStub()
+}
 
 describe('SignUpValidation Factory' , () => {
   test('Should call ValidationComposite with all validations', () => {
@@ -16,6 +27,7 @@ describe('SignUpValidation Factory' , () => {
       validations.push(new RequiredFieldValidation(requiredField))
     }
     validations.push(new FieldsComparisonValidation('password', 'passwordConfirmation'))
+    validations.push(new EmailValidation('email', makeEmailValidator()))
 
     expect(ValidationComposite).toHaveBeenCalledWith(validations)
   })
