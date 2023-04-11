c4b6f3eaede72cfadeadf1cf1edb53f712e91d9a
guimassoqueto <gui.massoqueto@gmail.com>
2023-04-11 06:16:19 -0300

    test: move password comparison to the ValidationComposite

--- tests/unit/factory.signup-validation-factory.test.ts
+++ tests/unit/factory.signup-validation-factory.test.ts
@@ -1,6 +1,9 @@
 import { makeSignUpValidation } from "../../src/main/factories/signup-validation"
 import { ValidationComposite } from "../../src/presentation/helpers/validators/validation-composite"
-import { RequiredFieldValidation } from "../../src/presentation/helpers/validators/required-field-validation"
+import { 
+  RequiredFieldValidation, 
+  FieldsComparisonValidation 
+} from "../../src/presentation/helpers/validators/validations/"
 import { Validation } from "../../src/presentation/helpers/validators/validation"
 
 jest.mock("../../src/presentation/helpers/validators/validation-composite")
@@ -9,9 +12,10 @@ describe('SignUpValidation Factory' , () => {
   test('Should call ValidationComposite with all validations', () => {
     makeSignUpValidation()
     const validations: Validation[] = []
-    for (const requiredField of ["name", "email", "password", "passwordConfirmation"]) {
-      validations.push(new RequiredFieldValidation(requiredField) )
+    for (const requiredField of ['name', 'email', 'password', 'passwordConfirmation']) {
+      validations.push(new RequiredFieldValidation(requiredField))
     }
+    validations.push(new FieldsComparisonValidation('password', 'passwordConfirmation'))
 
     expect(ValidationComposite).toHaveBeenCalledWith(validations)
   })
