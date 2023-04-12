5230c552476c0a3cea877dc5e4e19825316bc9ad
guimassoqueto <gui.massoqueto@gmail.com>
2023-04-12 15:22:24 -0300

    feat: ensure DbAuthentication call LoadAccountByEmailRepository w/ correct email

---
+++ src/data/protocols/load-account-by-email-repository.ts
@@ -0,0 +1,5 @@
+import { type AccountModel } from '../usecases/authentication/db-authentication-protocols'
+
+export interface LoadAccountByEmailRepository {
+  load: (email: string) => Promise<AccountModel>
+}
---
+++ src/data/usecases/authentication/db-authentication-usecase.ts
@@ -0,0 +1,12 @@
+import { type LoadAccountByEmailRepository } from '../../protocols/load-account-by-email-repository'
+import { type Authentication, type AuthenticationModel } from './db-authentication-protocols'
+
+export class DbAuthentication implements Authentication {
+  constructor (private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository) {}
+
+  async auth (authentication: AuthenticationModel): Promise<string | null> {
+    await this.loadAccountByEmailRepository.load(authentication.email)
+
+    return null
+  }
+}
