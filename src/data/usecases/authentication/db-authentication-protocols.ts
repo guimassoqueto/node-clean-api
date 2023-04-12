a553acda871c4a7680b4a7609692278bdd0a7b4d
guimassoqueto <gui.massoqueto@gmail.com>
2023-04-12 19:41:58 -0300

    feat: ensure DbAuthentication calls TokenGenerator with correct id

--- src/data/usecases/authentication/db-authentication-protocols.ts
+++ src/data/usecases/authentication/db-authentication-protocols.ts
@@ -1,3 +1,4 @@
 export * from '../../../domain/usecases/authentication'
 export * from '../../../domain/models/account'
 export * from '../../protocols/cryptography/hash-comparer'
+export * from '../../protocols/cryptography/token-generator'
--- src/data/usecases/authentication/db-authentication-usecase.ts
+++ src/data/usecases/authentication/db-authentication-usecase.ts
@@ -1,16 +1,18 @@
 import { type LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository'
-import { type Authentication, type AuthenticationModel, type HashComparer } from './db-authentication-protocols'
+import { type TokenGenerator, type Authentication, type AuthenticationModel, type HashComparer } from './db-authentication-protocols'
 
 export class DbAuthentication implements Authentication {
   constructor (
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
-    private readonly hashComparer: HashComparer
+    private readonly hashComparer: HashComparer,
+    private readonly tokenGenerator: TokenGenerator
   ) {}
 
   async auth (authentication: AuthenticationModel): Promise<string | null> {
     const account = await this.loadAccountByEmailRepository.load(authentication.email)
     if (account) {
       await this.hashComparer.compare(authentication.password, account.password)
+      await this.tokenGenerator.generate(account.id)
     }
     return null
   }
