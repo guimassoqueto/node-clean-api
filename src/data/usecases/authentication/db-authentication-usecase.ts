52e8910194a03386aab3e5ca2078ec60ee3fe7be
guimassoqueto <gui.massoqueto@gmail.com>
2023-04-12 20:25:38 -0300

    feat: ensure DbAuthentication call UpdateAccessTokenRepository w/ correct values

--- src/data/usecases/authentication/db-authentication-usecase.ts
+++ src/data/usecases/authentication/db-authentication-usecase.ts
@@ -1,11 +1,18 @@
-import { type LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository'
-import { type TokenGenerator, type Authentication, type AuthenticationModel, type HashComparer } from './db-authentication-protocols'
+import {
+  type LoadAccountByEmailRepository,
+  type TokenGenerator,
+  type Authentication,
+  type AuthenticationModel,
+  type HashComparer,
+  type UpdateAccessTokenRepository
+} from './db-authentication-protocols'
 
 export class DbAuthentication implements Authentication {
   constructor (
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
     private readonly hashComparer: HashComparer,
-    private readonly tokenGenerator: TokenGenerator
+    private readonly tokenGenerator: TokenGenerator,
+    private readonly uptadeAccessTokenRepository: UpdateAccessTokenRepository
   ) {}
 
   async auth (authentication: AuthenticationModel): Promise<string | null> {
@@ -15,8 +22,10 @@ export class DbAuthentication implements Authentication {
     const isMatch = await this.hashComparer.compare(authentication.password, account.password)
     if (!isMatch) return null
 
-    const Accesstoken = await this.tokenGenerator.generate(account.id)
+    const token = await this.tokenGenerator.generate(account.id)
 
-    return Accesstoken
+    await this.uptadeAccessTokenRepository.update(account.id, token)
+
+    return token
   }
 }
