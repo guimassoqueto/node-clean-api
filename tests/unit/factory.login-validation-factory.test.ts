04a706cd27c417c73cb4ae30da6b74b0fb4b0c7d
guimassoqueto <gui.massoqueto@gmail.com>
2023-04-11 21:06:56 -0300

    test: ensure LoginValidation factory return the correct validations

---
+++ tests/unit/factory.login-validation-factory.test.ts
@@ -0,0 +1,32 @@
+import { makeLoginValidation } from "../../src/main/factories/login/login-validation"
+import { ValidationComposite } from "../../src/presentation/helpers/validators/validation-composite"
+import { 
+  EmailValidation,
+  RequiredFieldValidation
+} from "../../src/presentation/helpers/validators/validations"
+import { Validation } from "../../src/presentation/helpers/validators/validation"
+import { EmailValidator } from "../../src/presentation/protocols"
+
+jest.mock("../../src/presentation/helpers/validators/validation-composite")
+
+function makeEmailValidator(): EmailValidator {
+  class EmailValidatorStub implements EmailValidator {
+    isValid (email: string) {
+      return true
+    }
+  }
+  return new EmailValidatorStub()
+}
+
+describe('LoginValidation Factory' , () => {
+  test('Should call ValidationComposite with all validations', () => {
+    makeLoginValidation()
+    const validations: Validation[] = []
+    for (const requiredField of ['email', 'password']) {
+      validations.push(new RequiredFieldValidation(requiredField))
+    }
+    validations.push(new EmailValidation('email', makeEmailValidator()))
+    
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
+  })
+})
