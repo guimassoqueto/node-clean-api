f3de338e845dd181a8b089bf356e2ccfa945ac04
guimassoqueto <gui.massoqueto@gmail.com>
2023-04-11 21:04:14 -0300

    feat: add LoginValidation factory

---
+++ src/main/factories/login/login-validation.ts
@@ -0,0 +1,22 @@
+import { ValidationComposite } from '../../../presentation/helpers/validators/validation-composite'
+import {
+  RequiredFieldValidation,
+  EmailValidation
+} from '../../../presentation/helpers/validators/validations'
+import { type Validation } from '../../../presentation/controllers/signup/signup-protocols'
+import { EmailValidatorAdapter } from '../../../utils/email-validator-adapter'
+
+export function makeLoginValidation (): ValidationComposite {
+  const validations: Validation[] = []
+
+  // validador de checagem se campos obrigatórios estão preenchidos
+  for (const requiredField of ['email', 'password']) {
+    validations.push(new RequiredFieldValidation(requiredField))
+  }
+
+  // validador de chacagem se o email fornecido na requisição é válido
+  const emailValidator = new EmailValidatorAdapter()
+  validations.push(new EmailValidation('email', emailValidator))
+
+  return new ValidationComposite(validations)
+}
--- src/presentation/controllers/login/login-controller.ts
+++ src/presentation/controllers/login/login-controller.ts
@@ -1,42 +1,24 @@
 import {
-  type EmailValidator,
-  type PasswordValidator,
   type Controller,
   type HttpRequest,
   type HttpResponse,
-  type Authentication
+  type Authentication,
+  type Validation
 } from './login-protocols'
-import { serverError, badRequest, ok, unauthorized } from '../../helpers/http-helper'
-import { InvalidParamError, MissingParamError } from '../../errors'
+import { serverError, ok, unauthorized, badRequest } from '../../helpers/http-helper'
 
 export class LoginController implements Controller {
   constructor (
-    private readonly emailValidator: EmailValidator,
-    private readonly passwordValidator: PasswordValidator,
-    private readonly authentication: Authentication
+    private readonly authentication: Authentication,
+    private readonly validation: Validation
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      const requiredFields: string[] = ['email', 'password']
-
-      // checa se os campos obrigatórios esperados estão presentes no corpo da requisição
-      for (const field of requiredFields) {
-        if (!(field in httpRequest.body)) {
-          return badRequest(new MissingParamError(field))
-        }
-      }
+      const error = this.validation.validate(httpRequest.body)
+      if (error) return badRequest(error)
 
       const { email, password } = httpRequest.body
-
-      // valida o email do usuário
-      const isValidEmail = this.emailValidator.isValid(email)
-      if (!isValidEmail) return badRequest(new InvalidParamError('email'))
-
-      // valida a senha do usuário
-      const isValidPassword = this.passwordValidator.isStrong(password)
-      if (!isValidPassword) return badRequest(new InvalidParamError('password'))
-
       const accessToken = await this.authentication.auth(email, password)
       if (!accessToken) return unauthorized()
 
