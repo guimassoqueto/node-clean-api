e9e6aeafad77f279735e485a123aa7d788f19f41
guimassoqueto <gui.massoqueto@gmail.com>
2023-04-13 19:21:28 -0300

    feat: ensure AddAccountMongoRepositoryu pdates the account accessToken

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,10 +1,11 @@
+import { ObjectId } from 'mongodb'
 import { type AddAccountRepository } from '../../../../data/protocols/db/add-account-repository'
-import { type LoadAccountByEmailRepository } from '../../../../data/usecases/authentication/db-authentication-protocols'
+import { type LoadAccountByEmailRepository, type UpdateAccessTokenRepository } from '../../../../data/usecases/authentication/db-authentication-protocols'
 import { type AccountModel } from '../../../../domain/models/account'
 import { type AddAccountModel } from '../../../../domain/usecases/add-account'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class AddAccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository {
+export class AddAccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository, UpdateAccessTokenRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(accountData)
@@ -18,5 +19,10 @@ export class AddAccountMongoRepository implements AddAccountRepository, LoadAcco
     const account = await accountCollection.findOne({ email })
 
     return MongoHelper.mapper<AccountModel>(account)
+  }
+
+  async updateAccessToken (id: string, token: string): Promise<void> {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.updateOne({ _id: new ObjectId(id) }, { $set: { accessToken: token } })
   }
 }
