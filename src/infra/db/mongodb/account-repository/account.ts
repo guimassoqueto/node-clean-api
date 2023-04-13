e022b14ffe87b9dcc0a8b42bd44e1da7cdff31ec
guimassoqueto <gui.massoqueto@gmail.com>
2023-04-13 18:01:27 -0300

    feat: add loadByEmail method

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,15 +1,21 @@
 import { type AddAccountRepository } from '../../../../data/protocols/db/add-account-repository'
+import { type LoadAccountByEmailRepository } from '../../../../data/usecases/authentication/db-authentication-protocols'
 import { type AccountModel } from '../../../../domain/models/account'
 import { type AddAccountModel } from '../../../../domain/usecases/add-account'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class AddAccountMongoRepository implements AddAccountRepository {
+export class AddAccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts')
-
     const result = await accountCollection.insertOne(accountData)
-
     const account = await accountCollection.findOne({ _id: result.insertedId })
+
+    return MongoHelper.mapper<AccountModel>(account)
+  }
+
+  async loadByEmail (email: string): Promise<AccountModel | null> {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    const account = await accountCollection.findOne({ email })
 
     return MongoHelper.mapper<AccountModel>(account)
   }
