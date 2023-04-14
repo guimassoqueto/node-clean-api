192ba5b0b658790fd0e237fa5efb73daec0f8856
guimassoqueto <gui.massoqueto@gmail.com>
2023-04-14 12:59:54 -0300

    test: add login route

--- tests/integration/main.route.login-routes.test.ts
+++ tests/integration/main.route.login-routes.test.ts
@@ -1,9 +1,11 @@
 import request from "supertest"
 import app from "../../src/main/config/app"
 import { MongoHelper } from "../../src/infra/db/mongodb/helpers/mongo-helper"
-import { MONGO_URL } from "../settings"
+import { MONGO_URL, SALT_ROUNDS } from "../settings"
+import { Collection } from "mongodb"
+import { hash } from "bcrypt"
 
-
+let accountCollection: Collection;
 describe('Login Route' , () => {
   beforeAll(async () => {
     await MongoHelper.connect(MONGO_URL);
@@ -14,7 +16,7 @@ describe('Login Route' , () => {
   })
 
   afterEach(async () => {
-    const accountCollection = await MongoHelper.getCollection("accounts")
+    accountCollection = await MongoHelper.getCollection("accounts")
     await accountCollection.deleteMany({})
   })
 
@@ -30,7 +32,29 @@ describe('Login Route' , () => {
         })
         .expect(200)
     })
+  })
 
+  describe('POST /login' , () => {
+    test('Should return 200 on login', async () => {
+      const rawPassword = "321!@#qweEWQ"
+      const passwordHash = await hash(rawPassword, SALT_ROUNDS)
+
+      const newUser: { name: string, email: string, password: string } = {
+        name: "any_user",
+        email: "any_email@email.com",
+        password: passwordHash
+      }
+
+      await accountCollection.insertOne(newUser)
+
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: newUser.email,
+          password: rawPassword
+        })
+        .expect(200)
+    })
   })
   
 })
