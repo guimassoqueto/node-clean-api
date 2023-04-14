52b56c905f4aa8314210fb8c23934d3c9139f769
guimassoqueto <gui.massoqueto@gmail.com>
2023-04-14 11:34:31 -0300

    feat: /login route signed to the app

--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,7 +1,9 @@
 import { type Router } from 'express'
 import { makeSignUpController } from '../factories/signup/signup-factory'
+import { makeLoginController } from '../factories/login/login-factory'
 import { expressRouteAdapter } from '../adapters/express'
 
 export default function (router: Router): void {
   router.post('/signup', expressRouteAdapter(makeSignUpController()))
+  router.post('/login', expressRouteAdapter(makeLoginController()))
 }
