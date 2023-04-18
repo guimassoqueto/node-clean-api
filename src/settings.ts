1db320704a50440447ee1b5b8f7b682bb50553bd
guimassoqueto <gui.massoqueto@gmail.com>
2023-04-18 16:02:11 -0300

    chore: update default variable value

--- src/settings.ts
+++ src/settings.ts
@@ -10,5 +10,5 @@ export const APP_PORT = parseInt(process.env.APP_PORT ?? '8000')
 export const JWT_SECRET = 'k123!@#-15_}{' // secredo usado para encriptar senhas e dados sigilosos
 export const SALT_ROUNDS = 12 // número de interaçoes na qual o segredo será aplicado no algoritmo de geração da hash. Recomenda-se qualquer valor acima de 10
 
-const MONGO_DB_NAME = process.env.MONGO_DB_NAME ?? 'test'
+const MONGO_DB_NAME = process.env.MONGO_DB_NAME ?? 'node-clean-api'
 export const MONGO_URL = process.env.MONGO_URL ?? `mongodb://${MONGO_ROOT_USERNAME}:${MONGO_ROOT_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB_NAME}?authSource=admin`
