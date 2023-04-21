7334ae83edbb00291430327044dbba23d527a61b
guimassoqueto <gui.massoqueto@gmail.com>
2023-04-21 07:59:42 -0300

    feat: add logger-factory

--- src/main/factories/logger/logger-factory.ts
+++ src/main/factories/logger/logger-factory.ts
@@ -1,3 +1,4 @@
+import { ENVIRONMENT } from '../../../settings'
 import winston, { format, type Logger } from 'winston'
 const { combine, timestamp, printf, label } = format
 
@@ -9,8 +10,17 @@ export default function loggerFactory (appLabel: string): Logger {
   // formato da mensagem: https://github.com/taylorhakes/fecha
   const timestampFormat = { format: 'DD-MM-YYYY Z HH:mm:ss.SSS' }
 
-  // TODO: Fazer level de acordo com a variavel de ambiente NODE_ENV
-  const level: string = 'debug'
+  // expressão regular que define qualquer frase que inicia com p, case-insensitive (P também é aceito)
+  // independente de ENVIROMENT ser "prod", "production", "PRod", etc vai identificar que o ambiente é de
+  // produção, pois inicia com a letra p
+  const regex: RegExp = /^p/i
+
+  let level: string
+  if (regex.test(ENVIRONMENT)) {
+    level = 'info'
+  } else {
+    level = 'debug'
+  }
 
   return winston.createLogger({
     level,
