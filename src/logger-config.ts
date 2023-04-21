a3911bf19bc1d086b1e68203fd279a7548e717fc
guimassoqueto <gui.massoqueto@gmail.com>
2023-04-21 15:46:34 -0300

    chore: change comment

--- src/logger-config.ts
+++ src/logger-config.ts
@@ -3,11 +3,11 @@ import winston, { format, type Logger } from 'winston'
 const { combine, timestamp, printf, label } = format
 
 export default function loggerConfig (appLabel: string): Logger {
-  // formato da mensage no logger
+  // formato da mensagem no logger
   const messageFormat: winston.Logform.Format = printf(({ level, message, label, timestamp }) => {
     return `${timestamp} [${label}][${level}]: ${message}`
   })
-  // formato da mensagem: https://github.com/taylorhakes/fecha
+  // formato do timestamp: https://github.com/taylorhakes/fecha
   const timestampFormat = { format: 'DD-MM-YYYY Z HH:mm:ss.SSS' }
 
   // expressão regular que define qualquer frase que inicia com p, case-insensitive (P também é aceito)
