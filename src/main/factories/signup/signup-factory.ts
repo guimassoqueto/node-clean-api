07606386d70770d69464489d68e52ab0b11e0fe8
guimassoqueto <gui.massoqueto@gmail.com>
2023-04-27 18:48:35 -0300

    feat: composites

--- src/main/factories/signup/signup-factory.ts
+++ src/main/factories/signup/signup-factory.ts
@@ -1,20 +1,30 @@
 import { SignUpControlller } from '../../../presentation/controllers/signup/signup-controller'
 import { DbAddAcccount } from '../../../data/usecases/add-account/db-add-account-usecase'
+import { DbAddUnverifiedAcccount } from '../../../data/usecases/add-unverified-account/db-add-unverified-account-usecase'
 import { BcryptAdapter } from '../../../infra/cryptography/bcrypt-adapter/bcrypt-adapter'
+import { JwtAdapter } from '../../../infra/cryptography/jwt-adapter/jwt-adapter'
 import { AccountMongoRepository } from '../../../infra/db/mongodb/account/account-mongo-repository'
+import { UnverifiedAccountMongoRepository } from '../../../infra/db/mongodb/account/unverified-account-mongo-repository'
 import { type Controller } from '../../../presentation/protocols'
 import { LoggingControllerDecorator } from '../../decorators/logging-controller-decorator'
 import { LoggingWinstonRepository } from '../../../infra/logger/winston/logging-winston-repository'
 import { makeSignUpValidation } from './signup-validation-factory'
-import { SALT_ROUNDS } from '../../../settings'
+import { SALT_ROUNDS, JWT_SECRET } from '../../../settings'
+import { makeEmailService } from './signup-email-service-factory'
 
 export function makeSignUpController (): Controller {
   const bcryptAdapter = new BcryptAdapter(SALT_ROUNDS)
   const addAccountMongoRepository = new AccountMongoRepository()
   const dbAddAcccount = new DbAddAcccount(bcryptAdapter, addAccountMongoRepository)
 
+  const encrypter = new JwtAdapter(JWT_SECRET)
+  const addUnverifiedAccountRepository = new UnverifiedAccountMongoRepository()
+  const dbAddUnverifiedAcccount = new DbAddUnverifiedAcccount(encrypter, addUnverifiedAccountRepository)
+
   const validationComposite = makeSignUpValidation()
-  const signUpControlller = new SignUpControlller(dbAddAcccount, validationComposite)
+  const emailService = makeEmailService()
+  const signUpControlller = new SignUpControlller(validationComposite, dbAddAcccount, dbAddUnverifiedAcccount, emailService)
+
   const loggingWinstonRepository = new LoggingWinstonRepository()
 
   return new LoggingControllerDecorator(signUpControlller, loggingWinstonRepository)
--- src/presentation/controllers/signup/signup-controller.ts
+++ src/presentation/controllers/signup/signup-controller.ts
@@ -3,15 +3,19 @@ import {
   type AddAccount,
   type HttpRequest,
   type HttpResponse,
-  type Validation
+  type Validation,
+  type EmailService,
+  type AddUnverifiedAccount
 } from './signup-controller-protocols'
 import { badRequest, ok, serverError, emailAlreadyInUse } from '../../helpers/http/http-helper'
 import { EmailAlreadyInUseError } from '../../errors'
 
 export class SignUpControlller implements Controller {
   constructor (
+    private readonly validation: Validation,
     private readonly addAccount: AddAccount,
-    private readonly validation: Validation
+    private readonly addUnverifiedAccount: AddUnverifiedAccount,
+    private readonly emailService: EmailService
   ) {}
 
   public async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -23,10 +27,14 @@ export class SignUpControlller implements Controller {
 
       const account = await this.addAccount.add({ name, email, password })
 
+      // encripta a id do usuário e prenche essa cript no banco
+      const accountIdHash = await this.addUnverifiedAccount.add(account.id)
+
+      await this.emailService.sendAccountVerificationEmail({ email, hash: accountIdHash.encryptedAccountId })
+
       return ok(account)
     } catch (error) {
       if (error instanceof EmailAlreadyInUseError) return emailAlreadyInUse()
-
       return serverError(error.stack)
     }
   }
