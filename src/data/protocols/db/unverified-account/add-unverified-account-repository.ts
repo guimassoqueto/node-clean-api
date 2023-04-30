7a8a3d898670453f5a8ad010f70b3bfad0277727
guimassoqueto <gui.massoqueto@gmail.com>
2023-04-30 06:30:01 -0300

    refactor: update param name

--- src/data/protocols/db/unverified-account/add-unverified-account-repository.ts
+++ src/data/protocols/db/unverified-account/add-unverified-account-repository.ts
@@ -1,5 +1,5 @@
 import { type UnverifiedAccountModel } from '../../../usecases/add-unverified-account/db-add-unverified-account-protocols'
 
 export interface AddUnverifiedAccountRepository {
-  add: (encryptedAccountId: string) => Promise<UnverifiedAccountModel>
+  add: (accountToken: string) => Promise<UnverifiedAccountModel>
 }
