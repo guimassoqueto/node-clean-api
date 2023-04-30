1fb8eb98f5ba38f023166de8d7b1e87aef6ef5e6
guimassoqueto <gui.massoqueto@gmail.com>
2023-04-30 07:32:38 -0300

    feat: implement 2 new interfaces related to accounts

--- src/infra/db/mongodb/account/account-mongo-repository.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.ts
@@ -1,12 +1,17 @@
 import { ObjectId } from 'mongodb'
-import { type AddAccountRepository } from '../../../../data/protocols/db/account/add-account-repository'
-import { type LoadAccountByEmailRepository, type UpdateAccessTokenRepository } from '../../../../data/usecases/authentication/db-authentication-protocols'
 import { type AccountModel } from '../../../../domain/models/account'
 import { type AddAccountModel } from '../../../../domain/usecases/add-account'
 import { MongoHelper } from '../helpers/mongo-helper'
 import { EmailAlreadyInUseError } from '../../../../presentation/errors'
+import {
+  type AddAccountRepository,
+  type LoadAccountByEmailRepository,
+  type UpdateAccessTokenRepository,
+  type LoadAccountByIdRepository,
+  type UpdateAccountVerifiedRepository
+} from '../../../../data/protocols/db/account'
 
-export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository, UpdateAccessTokenRepository {
+export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository, UpdateAccessTokenRepository, LoadAccountByIdRepository, UpdateAccountVerifiedRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts')
 
@@ -31,8 +36,21 @@ export class AccountMongoRepository implements AddAccountRepository, LoadAccount
     return MongoHelper.mapper<AccountModel>(account)
   }
 
+  async loadById (id: string): Promise<AccountModel | null> {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    const account = await accountCollection.findOne({ _id: new ObjectId(id) })
+    if (!account) return null
+
+    return MongoHelper.mapper<AccountModel>(account)
+  }
+
   async updateAccessToken (id: string, token: string): Promise<void> {
     const accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.updateOne({ _id: new ObjectId(id) }, { $set: { accessToken: token } })
+  }
+
+  async updateVerified (id: string, verified: boolean): Promise<void> {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.updateOne({ _id: new ObjectId(id) }, { $set: { verified } })
   }
 }
