c2badf8c806406e26bd3202bd4604c8189aeaf53
guimassoqueto <gui.massoqueto@gmail.com>
2023-04-30 08:46:48 -0300

    feat: add new interface to JwtAdapter

--- src/infra/cryptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.ts
@@ -1,11 +1,16 @@
-import { type Encrypter } from '../../../data/protocols/cryptography'
+import { type Encrypter, type Decrypter } from '../../../data/protocols/cryptography'
 import jwt from 'jsonwebtoken'
 
-export class JwtAdapter implements Encrypter {
+export class JwtAdapter implements Encrypter, Decrypter {
   constructor (private readonly jwtSecret: string) {}
 
   async encrypt (value: string): Promise<string> {
     const accessToken = jwt.sign({ id: value }, this.jwtSecret)
     return await new Promise(resolve => { resolve(accessToken) })
+  }
+
+  async decrypt (encryptedValue: string): Promise<object> {
+    const decriptedToken = jwt.decode(encryptedValue)
+    return await new Promise(resolve => { resolve(decriptedToken as object) })
   }
 }
