2f0d9d490eb178fbc9fa2da3d9f38c91ae83ceb3
guimassoqueto <gui.massoqueto@gmail.com>
2023-04-30 15:09:36 -0300

    test: create base test structure

---
+++ tests/integration/main.route.verify-account-routes.test.ts
@@ -0,0 +1,52 @@
+import { MongoHelper } from "../../src/infra/db/mongodb/helpers/mongo-helper";
+import app from "../../src/main/config/app"
+import request from "supertest"
+import { MONGO_URL, JWT_SECRET } from "../settings";
+import { Collection } from "mongodb";
+
+import { sign } from "jsonwebtoken"
+
+function makeAccount() {
+  return {
+    name: "any_name",
+    email: "any_email@email.com",
+    password: "any_password",
+    verified: false,
+    createdAt: new Date(2023, 11, 31),
+  }
+}
+
+type MakeUnverifiedAccount = [ object, string ]
+function makeUnverifiedAccount(accountId: {id: string}): MakeUnverifiedAccount {
+  const accountToken: string = sign(accountId, JWT_SECRET)
+  const unverifiedAccount: object = {
+    accountToken,
+    createdAt: new Date(2023, 11, 31)
+  }
+
+  return [unverifiedAccount, accountToken]
+}
+
+let accountsCollection: Collection
+let unverifiedAccountsCollection: Collection
+describe('Verify Account' , () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(MONGO_URL);
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  })
+
+  beforeEach(async () => {
+    accountsCollection = await MongoHelper.getCollection("accounts")
+    unverifiedAccountsCollection = await MongoHelper.getCollection("unverifiedAccounts")
+  })
+
+  afterEach(async () => {
+    await unverifiedAccountsCollection.deleteMany({})
+    await accountsCollection.deleteMany({})
+  })
+
+
+})
