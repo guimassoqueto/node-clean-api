327abb76e953747dd8420fde0c7273efce1be6df
guimassoqueto <gui.massoqueto@gmail.com>
2023-04-30 07:20:37 -0300

    feat: implement DbAccountVerification

---
+++ src/data/usecases/account-verification/db-account-verification-usecase.ts
@@ -0,0 +1,29 @@
+import {
+  type AccountVerification,
+  type Decrypter,
+  type LoadAccountByIdRepository,
+  type UpdateAccountVerifiedRepository,
+  type DeleteUnverifiedAccountByAccountTokenRepository
+} from './db-account-verification-protocols'
+
+export class DbAccountVerification implements AccountVerification {
+  constructor (
+    private readonly decrypter: Decrypter,
+    private readonly loadAccountByIdRepository: LoadAccountByIdRepository,
+    private readonly updateAccountVerified: UpdateAccountVerifiedRepository,
+    private readonly deleteUnverifiedAccountByAccountToken: DeleteUnverifiedAccountByAccountTokenRepository
+  ) {}
+
+  async verify (accountToken: string): Promise<boolean> {
+    const id = await this.decrypter.decryptAccountToken(accountToken)
+
+    const account = await this.loadAccountByIdRepository.loadById(id)
+    if (!account) return false
+
+    await this.updateAccountVerified.updateVerified(account.id, true)
+
+    await this.deleteUnverifiedAccountByAccountToken.deleteByAccountToken(accountToken)
+
+    return true
+  }
+}
