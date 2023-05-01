61dc7c47ceeb6d419b266999daad942a9923eb99
guimassoqueto <gui.massoqueto@gmail.com>
2023-05-01 17:09:01 -0300

    feat: update factory

--- src/main/factories/verify-account/verify-account-factory.ts
+++ src/main/factories/verify-account/verify-account-factory.ts
@@ -11,11 +11,13 @@ export function makeVerifyAccountController (): Controller {
   const validation = makeVerifyAccountValidation()
 
   const decrypter = new JwtAdapter(JWT_SECRET)
+  const encrypter = new JwtAdapter(JWT_SECRET)
   const loadAccountByIdRepository = new AccountMongoRepository()
   const updateAccountVerifiedRepository = new AccountMongoRepository()
   const changeAccountIdRepository = new AccountMongoRepository()
+  const updateAccessTokenRepository = new AccountMongoRepository()
   const deleteUnverifiedAccountByAccountToken = new UnverifiedAccountMongoRepository()
-  const dbAccountVerification = new DbAccountVerification(decrypter, loadAccountByIdRepository, updateAccountVerifiedRepository, changeAccountIdRepository, deleteUnverifiedAccountByAccountToken)
+  const dbAccountVerification = new DbAccountVerification(decrypter, encrypter, loadAccountByIdRepository, updateAccountVerifiedRepository, changeAccountIdRepository, updateAccessTokenRepository, deleteUnverifiedAccountByAccountToken)
 
   return new VerifyAccountController(validation, dbAccountVerification)
 }
