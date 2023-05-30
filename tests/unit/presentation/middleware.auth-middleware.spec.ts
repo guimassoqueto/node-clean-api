ac8bb1a18b8245a80804dde2132f178be10ed3f1
guimassoqueto <gui.massoqueto@gmail.com>
2023-05-30 16:49:56 -0300

    test: ensure AuthMIddleware calls LoadAccountByToken with correct token

--- tests/unit/presentation/middleware.auth-middleware.spec.ts
+++ tests/unit/presentation/middleware.auth-middleware.spec.ts
@@ -2,21 +2,69 @@ import { HttpRequest } from "../../../src/presentation/protocols"
 import { AuthMiddleware } from "../../../src/presentation/middlewares/auth-middleware"
 import { forbidden } from "../../../src/presentation/helpers/http/http-helper"
 import { AccessDeniedError } from "../../../src/errors"
+import { LoadAccountByToken } from "../../../src/domain/usecases/load-account-by-token"
+import { AccountModel } from "../../../src/domain/models/account"
 
 function makeFakeRequest(): HttpRequest {
   return {
-    headers: {},
+    headers: {
+      'x-access-token': 'any-token'
+    },
     body: {}
+  }
+}
+
+function makeLoadAccountByToken(): LoadAccountByToken {
+  function makeFakeAccount(): AccountModel {
+    return {
+      id: "any-id",
+      name: "any-name",
+      email: "any-email@gmail.com",
+      password: "any-password",
+      verified: true,
+      createdAt: new Date(2023, 11, 31)
+    }
+  }
+
+  class LoadAccountByTokenStub implements LoadAccountByToken {
+    async load (accessToken: string, role?: string | undefined): Promise<AccountModel> {
+      return new Promise(resolve => resolve(makeFakeAccount()))
+    }
+  }
+
+  return new LoadAccountByTokenStub()
+}
+
+type SutType = {
+  sut: AuthMiddleware,
+  loadAccountByTokenStub: LoadAccountByToken
+}
+
+function makeSut(): SutType {
+  const loadAccountByTokenStub = makeLoadAccountByToken()
+  const sut = new AuthMiddleware(loadAccountByTokenStub)
+  return {
+    sut,
+    loadAccountByTokenStub
   }
 }
 
 describe('AuthMiddleware' , () => {
   test('Should return 403 if no x-access-token exists in headers', async () => {
-    const sut = new AuthMiddleware()
+    const { sut } = makeSut()
     const request = makeFakeRequest()
     delete request.headers
     const response = await sut.handle(request)
 
     expect(response).toEqual(forbidden(new AccessDeniedError()))
+  })
+
+  test('Shoul call LoadaAccountByToken with correct accessToken', async () => {
+    const { sut, loadAccountByTokenStub } = makeSut()
+    const loadSpy = jest.spyOn(loadAccountByTokenStub, "load")
+    const request = makeFakeRequest()
+    await sut.handle(request)
+
+    expect(loadSpy).toHaveBeenCalledWith('any-token')
   })
 })
