60721d9216a4adc14bace7f28584f1bbc070f8b6
guimassoqueto <gui.massoqueto@gmail.com>
2023-05-31 15:23:17 -0300

    fix: update jest config files

--- jest-unit.config.ts
+++ jest-unit.config.ts
@@ -3,6 +3,6 @@ import config from "./jest.config";
 const { testMatch, ...rest } = config;
 
 export default {
-  testMatch: [ "**/unit/*.(spec|test).ts" ],
+  testMatch: [ "**/unit/**/*.(spec|test).ts" ],
   ...rest
 }
