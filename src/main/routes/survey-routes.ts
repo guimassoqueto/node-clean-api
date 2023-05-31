bca4815079403c583a884921ba7e0098354ef89b
guimassoqueto <gui.massoqueto@gmail.com>
2023-05-31 10:32:27 -0300

    feat: add makeAuthMiddleware and assign it to the /surveys route

--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -1,7 +1,13 @@
 import { type Router } from 'express'
 import { expressRouteAdapter } from '../adapters/express-route-adapter'
+import { expressMiddlewareAdapter } from '../adapters/express-middleware-adapter'
 import { makeAddSurveyController } from '../factories/controllers/surveys/add-survey/add-survey-controller-factory'
+import { makeAuthMiddleware } from '../factories/middlewares/auth-middleware-factory'
 
 export default function (router: Router): void {
-  router.post('/surveys', expressRouteAdapter(makeAddSurveyController()))
+  router.post(
+    '/surveys',
+    expressMiddlewareAdapter(makeAuthMiddleware('ADMIN')),
+    expressRouteAdapter(makeAddSurveyController())
+  )
 }
