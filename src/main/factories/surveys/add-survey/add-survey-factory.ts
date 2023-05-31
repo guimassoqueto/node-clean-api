e47ee5476f1296ab8f183fcfb3d77fb8736b6ffd
guimassoqueto <gui.massoqueto@gmail.com>
2023-05-31 08:09:12 -0300

    refactor: move survey-related controllers and update imports

---
+++ src/main/factories/surveys/add-survey/add-survey-factory.ts
@@ -0,0 +1,12 @@
+import { type Controller } from '../../../../presentation/protocols'
+import { AddSurveyController } from '../../../../presentation/controllers/survey/add-survey-controller'
+import { makeAddSurveyValidation } from './add-survey-validation-factory'
+import { SurveyMongoRepository } from '../../../../infra/db/mongodb/survey/survey-mongo-repository'
+import { DbAddSurvey } from '../../../../data/usecases/add-survey/db-add-survey-usecase'
+
+export function makeAddSurveyController (): Controller {
+  const validations = makeAddSurveyValidation()
+  const addSurveyRepository = new SurveyMongoRepository()
+  const dbAddSurvey = new DbAddSurvey(addSurveyRepository)
+  return new AddSurveyController(validations, dbAddSurvey)
+}
--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -1,6 +1,6 @@
 import { type Router } from 'express'
 import { expressRouteAdapter } from '../adapters/express-route-adapter'
-import { makeAddSurveyController } from '../factories/add-survey/add-survey-factory'
+import { makeAddSurveyController } from '../factories/surveys/add-survey/add-survey-factory'
 
 export default function (router: Router): void {
   router.post('/surveys', expressRouteAdapter(makeAddSurveyController()))
