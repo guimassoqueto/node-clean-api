af2047ae65cb594d7f3aa519bd338b8e529c027b
guimassoqueto <gui.massoqueto@gmail.com>
2023-05-31 09:52:10 -0300

    chore: remove useless comment

--- src/main/adapters/express-route-adapter.ts
+++ src/main/adapters/express-route-adapter.ts
@@ -1,12 +1,7 @@
 import { type Controller, type HttpRequest, type HttpResponse } from '../../presentation/protocols'
 import { type Request, type Response } from 'express'
 
-/**
- * Função que recebe uma classe que implementa o método Controller
- * e adapta os parâmetros para um formato no qual o express espera receber,
- * nest caso o formato HttpRequest do controller deve ser modificado para se enquadrar
- * no formato Request do express
- */
+// Design Pattern: Proxy
 export function expressRouteAdapter (controller: Controller) {
   return async (req: Request, res: Response) => {
     const httpRequest: HttpRequest = {
