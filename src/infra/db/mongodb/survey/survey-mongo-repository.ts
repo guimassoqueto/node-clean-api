2e04a009172520edf754b8e8dd6a7e15d1cb881d
guimassoqueto <gui.massoqueto@gmail.com>
2023-06-01 07:57:44 -0300

    feat: implement loadSurveysRepository on SurveyMongoRepository

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,11 +1,21 @@
-import { type AddSurveyRepository } from '../../../../data/protocols/db/survey'
+import { type AddSurveyRepository, type LoadSurveysRepository } from '../../../../data/protocols/db/survey'
 import { type AddSurveyModel } from '../../../../data/usecases/add-survey/db-add-survey-protocols'
+import { type SurveyModel } from '../../../../domain/models/survey'
 import { MongoHelper } from '../../../db/mongodb/helpers/mongo-helper'
 
-export class SurveyMongoRepository implements AddSurveyRepository {
+export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository {
   async add (surveyData: AddSurveyModel): Promise<void> {
     const mongo = MongoHelper.getInstance()
     const surveyCollection = await mongo.getCollection('surveys')
     await surveyCollection.insertOne(surveyData)
+  }
+
+  async loadAll (): Promise<SurveyModel[]> {
+    const mongo = MongoHelper.getInstance()
+    const surveyCollection = await mongo.getCollection('surveys')
+    const allSurveys = await surveyCollection.find().toArray()
+    return allSurveys.map((survey) => {
+      return mongo.mapper<SurveyModel>(survey)
+    })
   }
 }
