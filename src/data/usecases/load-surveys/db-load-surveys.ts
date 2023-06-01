565e0cdd2492da6771a50e217fb9ad8cb9460304
guimassoqueto <gui.massoqueto@gmail.com>
2023-06-01 07:09:37 -0300

    feat: ensure DbLoadSurveys  returns a valid list on LoadSurveysRepository succes

--- src/data/usecases/load-surveys/db-load-surveys.ts
+++ src/data/usecases/load-surveys/db-load-surveys.ts
@@ -10,7 +10,7 @@ export class DbLoadSurveys implements LoadSurveys {
   ) {}
 
   async load (): Promise<SurveyModel[]> {
-    await this.loadSurveysRepository.load()
-    return await new Promise(resolve => { resolve([]) })
+    const surveys = await this.loadSurveysRepository.load()
+    return surveys
   }
 }
