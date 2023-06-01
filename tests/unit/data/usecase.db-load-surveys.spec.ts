8698e501e897a226606d517477397454dfc7c61b
guimassoqueto <gui.massoqueto@gmail.com>
2023-06-01 07:10:05 -0300

    test: ensure DbLoadSurveys  returns a valid list on LoadSurveysRepository succes

--- tests/unit/data/usecase.db-load-surveys.spec.ts
+++ tests/unit/data/usecase.db-load-surveys.spec.ts
@@ -50,4 +50,11 @@ describe('DbLoadSurveys' , () => {
 
     expect(spyLoad).toHaveBeenCalled()
   })
+
+  test('Should return a list of Surveys on LoadSurveysRepository success', async () => {
+    const { sut } = makeSut()
+    const surveys = await sut.load()
+
+    expect(surveys).toEqual(makeFakeSurveys())
+  })
 })
