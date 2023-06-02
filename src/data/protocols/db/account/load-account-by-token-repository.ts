c9e0061c5a99bb8414f3ad2032e4f3a46feefb04
guimassoqueto <gui.massoqueto@gmail.com>
2023-06-02 07:26:20 -0300

    refactor: ../../../usecases -> @src/data/usecases

--- src/data/protocols/db/account/load-account-by-token-repository.ts
+++ src/data/protocols/db/account/load-account-by-token-repository.ts
@@ -1,6 +1,6 @@
 import {
   type AccountModel
-} from '../../../usecases/load-account-by-token/db-load-account-by-token-protocols'
+} from '@src/data/usecases/load-account-by-token/db-load-account-by-token-protocols'
 
 export interface LoadAccountByTokenRepository {
   loadByToken: (accessToken: string, role?: string) => Promise<AccountModel | null>
