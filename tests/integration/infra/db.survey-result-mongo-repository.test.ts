f081037c37fa4febf29c38277b369b4c882e7db3
guimassoqueto <gui.massoqueto@gmail.com>
2023-06-05 13:23:28 -0300

    test: ensure SurveyResultMongoRepository insert new surveyResult it is new

---
+++ tests/integration/infra/db.survey-result-mongo-repository.test.ts
@@ -0,0 +1,103 @@
+import { SurveyResultMongoRepository } from '@src/infra/db/mongodb/survey-result/survey-result-mongo-repository'
+import { MONGO_URL } from "@tests/settings"
+import { MongoHelper } from "@src/infra/db/mongodb/helpers/mongo-helper"
+import { Collection } from "mongodb"
+import { AddSurveyModel } from '@src/domain/usecases/add-survey';
+import { SurveyModel } from '@src/domain/models/survey';
+import { AddAccountModel } from '@src/domain/usecases/add-account';
+import { AccountModel } from '@src/domain/models/account';
+
+const RealDate = Date;
+class MockDate extends RealDate {
+  constructor() {
+    super('2030-01-01T00:00:00Z');
+  }
+}
+
+let surveyCollection: Collection
+let surveyResultCollection: Collection
+let accountCollection: Collection
+let mongo: MongoHelper 
+
+function makeSurvey(): AddSurveyModel {
+  return {
+    createdAt: new Date(),
+    question: `any-question`,
+    answers: [
+      {
+        image:'image1',
+        answer: 'any-answer1'
+      },
+      {
+        answer: 'any-answer2'
+      },
+    ]
+  }
+}
+
+function makeAccount(): AddAccountModel {
+  return {
+    name: "any-name",
+    email: "any-emal",
+    password: "any-password"
+  }
+}
+
+
+async function createDbSurvey(): Promise<SurveyModel> {
+  const newSurvey = await surveyCollection.insertOne(makeSurvey())
+  const survey = await surveyCollection.findOne({ _id: newSurvey.insertedId })
+  return mongo.mapper<SurveyModel>(survey)
+}
+
+async function createDbAccount(): Promise<AccountModel> {
+  const newAccount = await accountCollection.insertOne(makeAccount())
+  const account = await accountCollection.findOne({ _id: newAccount.insertedId })
+  return mongo.mapper<AccountModel>(account)
+}
+
+describe('SurveyResultMongoRepository' , () => {
+  beforeAll(async () => {
+    (global as any).Date = MockDate;
+    mongo = MongoHelper.getInstance()
+    await mongo.connect(MONGO_URL)
+  })
+
+  afterAll(async () => {
+    (global as any).Date = RealDate;
+    await mongo.disconnect();
+  })
+
+  beforeEach(async () => {
+    surveyCollection = await mongo.getCollection("surveys")
+    accountCollection = await mongo.getCollection("accounts")
+    surveyResultCollection = await mongo.getCollection("surveyResults")
+    await surveyCollection.deleteMany({})
+    await accountCollection.deleteMany({})
+    await surveyResultCollection.deleteMany({})
+  })
+
+  afterEach(async () => {
+    await surveyCollection.deleteMany({})
+    await accountCollection.deleteMany({})
+    await surveyResultCollection.deleteMany({})
+  })
+
+  describe('save()' , () => {
+    test('Should add a survey result if its new', async () => {
+      const sut = new SurveyResultMongoRepository()
+      const survey = await createDbSurvey()
+      const account = await createDbAccount()
+      const surveyResult = await sut.save({
+        surveyId: survey.id,
+        answer: survey.answers[0].answer,
+        accountId: account.id,
+        date: new Date(2030, 11, 31)
+      })
+
+      expect(surveyResult).toBeTruthy()
+      expect(surveyResult.id).toBeTruthy()
+      expect(surveyResult.answer).toEqual(survey.answers[0].answer)
+    })
+  })
+})
