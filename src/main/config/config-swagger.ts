4c47d39b54dd6c8e646fd0d15994be018d046dd2
guimassoqueto <gui.massoqueto@gmail.com>
2023-06-08 07:23:48 -0300

    feat: add noCache middleware to swagger /api-docs route

--- src/main/config/config-swagger.ts
+++ src/main/config/config-swagger.ts
@@ -1,7 +1,8 @@
 import { type Express } from 'express'
+import { noCache } from '@src/main/middlewares'
 import swagger from 'swagger-ui-express'
 import swaggerDoc from '@src/main/docs'
 
 export default function (app: Express): void {
-  app.use('/api-docs', swagger.serve, swagger.setup(swaggerDoc))
+  app.use('/api-docs', noCache, swagger.serve, swagger.setup(swaggerDoc))
 }
