d74422776d2abbc407f33b8895f9c0a72057e9c9
guimassoqueto <gui.massoqueto@gmail.com>
2023-06-20 16:05:10 -0300

    feat: ensure DbLoadSurveyResult returns a surveyResult on success

--- src/data/usecases/survey-result/load-survey-result/db-load-survey-result.ts
+++ src/data/usecases/survey-result/load-survey-result/db-load-survey-result.ts
@@ -11,6 +11,6 @@ export class DbLoadSurveyResult implements LoadSurveyResult {
 
   async load (surveyId: string): Promise<SurveyResultModel> {
     const surveyResult = await this.loadSurveyResultRepository.loadBySurveyId(surveyId)
-    return await Promise.resolve(surveyResult)
+    return surveyResult
   }
 }
