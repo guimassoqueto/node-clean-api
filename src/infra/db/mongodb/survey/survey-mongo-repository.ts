072fcc792571bcddf4b29a9a0aea2c88d87ffecb
guimassoqueto <gui.massoqueto@gmail.com>
2023-06-20 10:51:53 -0300

    refactor: use build design pattern to build the aggregation query

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,14 +1,18 @@
 import {
   type AddSurveyRepository,
-  type LoadSurveysRepository,
-  type LoadSurveyByIdRepository
+  type LoadSurveyByIdRepository,
+  type LoadSurveysRepository
 } from '@src/data/protocols/db/survey'
 import { type AddSurveyParams } from '@src/data/usecases/survey/add-survey/db-add-survey-protocols'
 import { type SurveyModel } from '@src/domain/models/survey'
-import { MongoHelper } from '@src/infra/db/mongodb/helpers/mongo-helper'
+import { MongoHelper } from '@src/infra/db/mongodb/helpers'
 import { ObjectId } from 'mongodb'
 
-export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository, LoadSurveyByIdRepository {
+export class SurveyMongoRepository
+implements
+    AddSurveyRepository,
+    LoadSurveysRepository,
+    LoadSurveyByIdRepository {
   async add (surveyData: AddSurveyParams): Promise<void> {
     const mongo = MongoHelper.getInstance()
     const surveyCollection = await mongo.getCollection('surveys')
