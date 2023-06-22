5411f50beb5497fd57540d35dfaffd4bd303463d
guimassoqueto <gui.massoqueto@gmail.com>
2023-06-22 09:14:21 -0300

    refactor: change factory name

--- tests/unit/presentation/controller.login-controller.spec.ts
+++ tests/unit/presentation/controller.login-controller.spec.ts
@@ -1,106 +1,113 @@
-import { LoginController } from '@src/presentation/controllers/account/login/login-controller'
-import { Authentication, Validation, AuthenticationParams } from '@src/presentation/controllers/account/login/login-controller-protocols'
-import { badRequest, ok, serverError, unauthorized } from '@src/presentation/helpers/http/http-helper'
-import { HttpRequest } from '@src/presentation/protocols';
-import { MissingParamError  } from '@src/errors'
-import { mockValidation } from '@tests/helpers'
+import { LoginController } from "@src/presentation/controllers/account/login/login-controller";
+import {
+  Authentication,
+  AuthenticationParams,
+  Validation,
+} from "@src/presentation/controllers/account/login/login-controller-protocols";
+import {
+  badRequest,
+  ok,
+  serverError,
+  unauthorized,
+} from "@src/presentation/helpers/http/http-helper";
+import { HttpRequest } from "@src/presentation/protocols";
+import { MissingParamError } from "@src/errors";
+import { mockValidation } from "@tests/helpers";
 
-
-function mockAuthentication(accessToken: string = 'any-token'): Authentication {
+function mockAuthentication(accessToken: string = "any-token"): Authentication {
   class AuthenticationStub implements Authentication {
     async auth(authentication: AuthenticationParams): Promise<string | null> {
-      return Promise.resolve(accessToken)
+      return Promise.resolve(accessToken);
     }
   }
-  return new AuthenticationStub()
+  return new AuthenticationStub();
 }
-
 
 function mockAuthenticationParams(): AuthenticationParams {
   return {
-    email: 'any-email',
-    password: 'any-password'
-  }
+    email: "any-email",
+    password: "any-password",
+  };
 }
 
-const accessToken = 'any-access-token'
+const accessToken = "any-access-token";
 
-type SutTypes =  {
-  sut: LoginController
-  validationStub: Validation,
-  authenticationStub: Authentication
-}
+type SutTypes = {
+  sut: LoginController;
+  validationStub: Validation;
+  authenticationStub: Authentication;
+};
 
 function makeSut(): SutTypes {
-  const validationStub = mockValidation()
-  const authenticationStub = mockAuthentication(accessToken)
+  const validationStub = mockValidation();
+  const authenticationStub = mockAuthentication(accessToken);
 
-  const sut = new LoginController(authenticationStub, validationStub)
+  const sut = new LoginController(authenticationStub, validationStub);
 
   return {
     sut,
     authenticationStub,
-    validationStub
-  }
+    validationStub,
+  };
 }
 
-function makeFakeRequest(): HttpRequest {
+function mockRequest(): HttpRequest {
   return {
-    body: mockAuthenticationParams()
-  }
+    body: mockAuthenticationParams(),
+  };
 }
 
-describe('Login Controller' , () => {
-  test('Should call Authentication with correct values', async () => {
-    const { sut, authenticationStub } = makeSut()
-    const spyAuth = jest.spyOn(authenticationStub, 'auth')
-    const httpRequest = makeFakeRequest()
-    const { email, password } = httpRequest.body
-    await sut.handle(httpRequest)
+describe("Login Controller", () => {
+  test("Should call Authentication with correct values", async () => {
+    const { sut, authenticationStub } = makeSut();
+    const spyAuth = jest.spyOn(authenticationStub, "auth");
+    const httpRequest = mockRequest();
+    const { email, password } = httpRequest.body;
+    await sut.handle(httpRequest);
 
-    expect(spyAuth).toHaveBeenCalledWith({ email, password })
-  })
+    expect(spyAuth).toHaveBeenCalledWith({ email, password });
+  });
 
-  test('Should return 401 if invalid credentials are provided', async () => {
-    const { sut, authenticationStub } = makeSut()
-    jest.spyOn(authenticationStub, 'auth').mockResolvedValueOnce(null)
-    const httpResponse = await sut.handle(makeFakeRequest())
+  test("Should return 401 if invalid credentials are provided", async () => {
+    const { sut, authenticationStub } = makeSut();
+    jest.spyOn(authenticationStub, "auth").mockResolvedValueOnce(null);
+    const httpResponse = await sut.handle(mockRequest());
 
-    expect(httpResponse).toEqual(unauthorized())
-  })
+    expect(httpResponse).toEqual(unauthorized());
+  });
 
-  test('Should return 500 in authentication throws', async () => {
-    const { sut, authenticationStub } = makeSut()
-    const error = new Error('Some error')
-    jest.spyOn(authenticationStub, 'auth').mockRejectedValueOnce(error)
-    const httpResponse = await sut.handle(makeFakeRequest())
+  test("Should return 500 in authentication throws", async () => {
+    const { sut, authenticationStub } = makeSut();
+    const error = new Error("Some error");
+    jest.spyOn(authenticationStub, "auth").mockRejectedValueOnce(error);
+    const httpResponse = await sut.handle(mockRequest());
 
-    expect(httpResponse).toEqual(serverError(error))
-  })
+    expect(httpResponse).toEqual(serverError(error));
+  });
 
-  test('Should return 200 if authentication succeed', async () => {
-    const { sut } = makeSut()
-    const httpResponse = await sut.handle(makeFakeRequest())
+  test("Should return 200 if authentication succeed", async () => {
+    const { sut } = makeSut();
+    const httpResponse = await sut.handle(mockRequest());
 
-    expect(httpResponse).toEqual(ok({ accessToken }))
-  })
+    expect(httpResponse).toEqual(ok({ accessToken }));
+  });
 
-  test('Should call Validation with correct value', async () => {
-    const { sut, validationStub } = makeSut()
-    const validateSpy = jest.spyOn(validationStub, 'validate')
-    const httpRequest = makeFakeRequest()
+  test("Should call Validation with correct value", async () => {
+    const { sut, validationStub } = makeSut();
+    const validateSpy = jest.spyOn(validationStub, "validate");
+    const httpRequest = mockRequest();
     await sut.handle(httpRequest);
 
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body);
-  })
+  });
 
-  test('Should return 400 if Validation return an error', async () => {
-    const { sut,validationStub } = makeSut()
-    const error = new MissingParamError('any_field')
-    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(error)
-    const httpRequest = makeFakeRequest()
-    const httpResponse = await sut.handle(httpRequest)
+  test("Should return 400 if Validation return an error", async () => {
+    const { sut, validationStub } = makeSut();
+    const error = new MissingParamError("any_field");
+    jest.spyOn(validationStub, "validate").mockReturnValueOnce(error);
+    const httpRequest = mockRequest();
+    const httpResponse = await sut.handle(httpRequest);
 
-    expect(httpResponse).toEqual(badRequest(error))
-  })
-})
+    expect(httpResponse).toEqual(badRequest(error));
+  });
+});
--- tests/unit/presentation/controller.save-survey-result.spec.ts
+++ tests/unit/presentation/controller.save-survey-result.spec.ts
@@ -1,163 +1,170 @@
-import { SaveSurveyResultController } from '@src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller'
-import { HttpRequest } from '@src/presentation/protocols'
-import { forbidden, serverError, ok } from '@src/presentation/helpers/http'
-import { 
-  LoadSurveyById, SurveyModel, InvalidParamError,
-  SaveSurveyResult, SaveSurveyResultParams, SurveyResultModel
-} from '@src/presentation/controllers/survey-result/save-survey-result/save-survey-result-protocols'
-import { RealDate, MockDate, mockSurveyModel } from '@tests/helpers'
-
+import { SaveSurveyResultController } from "@src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller";
+import { HttpRequest } from "@src/presentation/protocols";
+import { forbidden, ok, serverError } from "@src/presentation/helpers/http";
+import {
+  InvalidParamError,
+  LoadSurveyById,
+  SaveSurveyResult,
+  SaveSurveyResultParams,
+  SurveyModel,
+  SurveyResultModel,
+} from "@src/presentation/controllers/survey-result/save-survey-result/save-survey-result-protocols";
+import { MockDate, mockSurveyModel, RealDate } from "@tests/helpers";
 
 function mockSaveSurveyResult(): SaveSurveyResult {
   class SaveSurveyResultStub implements SaveSurveyResult {
-    async save (data: SaveSurveyResultParams): Promise<SurveyResultModel> {
-      return Promise.resolve(mockSurveyResult())
+    async save(data: SaveSurveyResultParams): Promise<SurveyResultModel> {
+      return Promise.resolve(mockSurveyResult());
     }
   }
-  return new SaveSurveyResultStub()
+  return new SaveSurveyResultStub();
 }
 
 function mockLoadSurveyById(): LoadSurveyById {
   class LoadSurveyByIdStub implements LoadSurveyById {
-    async loadById (id: string) : Promise<SurveyModel | null> {
-      return  Promise.resolve(mockSurveyModel())
+    async loadById(id: string): Promise<SurveyModel | null> {
+      return Promise.resolve(mockSurveyModel());
     }
   }
-  return new LoadSurveyByIdStub()
+  return new LoadSurveyByIdStub();
 }
 
 type SutTypes = {
-  sut: SaveSurveyResultController
-  loadSurveyByIdStub: LoadSurveyById
-  saveSurveyResultStub: SaveSurveyResult
-}
+  sut: SaveSurveyResultController;
+  loadSurveyByIdStub: LoadSurveyById;
+  saveSurveyResultStub: SaveSurveyResult;
+};
 
 function makeSut(): SutTypes {
-  const loadSurveyByIdStub = mockLoadSurveyById()
-  const saveSurveyResultStub = mockSaveSurveyResult()
-  const sut = new SaveSurveyResultController(loadSurveyByIdStub, saveSurveyResultStub)
+  const loadSurveyByIdStub = mockLoadSurveyById();
+  const saveSurveyResultStub = mockSaveSurveyResult();
+  const sut = new SaveSurveyResultController(
+    loadSurveyByIdStub,
+    saveSurveyResultStub,
+  );
 
   return {
     sut,
     loadSurveyByIdStub,
-    saveSurveyResultStub
-  }
+    saveSurveyResultStub,
+  };
 }
 
-function makeRequest(): HttpRequest {
-  const answers = mockSurveyModel().answers
+function mockRequest(): HttpRequest {
+  const answers = mockSurveyModel().answers;
   const answer = answers[Math.floor(Math.random() * answers.length)].answer;
   return {
     params: {
-      surveyId: 'any-id'
+      surveyId: "any-id",
     },
     body: {
-      answer
+      answer,
     },
-    accountId: 'any-account-id'
-  }
+    accountId: "any-account-id",
+  };
 }
 
 function mockSurveyResult(): SurveyResultModel {
   return {
-    surveyId: 'valid-survey-id',
-    question: 'valid-question',
+    surveyId: "valid-survey-id",
+    question: "valid-question",
     answers: [
       {
-        image: 'any-image-1',
-        answer: 'any-answer-1',
+        image: "any-image-1",
+        answer: "any-answer-1",
         count: 50,
-        percent: 50
+        percent: 50,
       },
       {
-        image: 'any-image-2',
-        answer: 'any-answer-2',
+        image: "any-image-2",
+        answer: "any-answer-2",
         count: 50,
-        percent: 50
-      }
+        percent: 50,
+      },
     ],
-    date: new Date(2030, 11, 31)
-  }
+    date: new Date(2030, 11, 31),
+  };
 }
 
-describe('SaveSurveyResultController' , () => {
-
+describe("SaveSurveyResultController", () => {
   beforeAll(() => {
     (global as any).Date = MockDate;
-  })
+  });
 
   afterAll(() => {
     (global as any).Date = RealDate;
-  })
+  });
 
-  test('Should call LoadSurveyById with correct args', async () => {
-    const { sut, loadSurveyByIdStub } = makeSut()
-    const loadByIdSpy = jest.spyOn(loadSurveyByIdStub, 'loadById')
-    const request = makeRequest()
+  test("Should call LoadSurveyById with correct args", async () => {
+    const { sut, loadSurveyByIdStub } = makeSut();
+    const loadByIdSpy = jest.spyOn(loadSurveyByIdStub, "loadById");
+    const request = mockRequest();
 
-    await sut.handle(request)
-    expect(loadByIdSpy).toHaveBeenCalledWith(request.params.surveyId)
-  })
+    await sut.handle(request);
+    expect(loadByIdSpy).toHaveBeenCalledWith(request.params.surveyId);
+  });
 
-  test('Should return 401 if surveyId does not exist', async () => {
-    const { sut, loadSurveyByIdStub } = makeSut()
-    jest.spyOn(loadSurveyByIdStub, 'loadById').mockResolvedValueOnce(null)
-    const request = makeRequest()
+  test("Should return 401 if surveyId does not exist", async () => {
+    const { sut, loadSurveyByIdStub } = makeSut();
+    jest.spyOn(loadSurveyByIdStub, "loadById").mockResolvedValueOnce(null);
+    const request = mockRequest();
 
-    const response = await sut.handle(request)
-    expect(response).toStrictEqual(forbidden(new InvalidParamError('surveyId')))
-  })
+    const response = await sut.handle(request);
+    expect(response).toStrictEqual(
+      forbidden(new InvalidParamError("surveyId")),
+    );
+  });
 
-  test('Should return 500 if surveyId throws', async () => {
-    const { sut, loadSurveyByIdStub } = makeSut()
-    const error = new Error()
-    jest.spyOn(loadSurveyByIdStub, 'loadById').mockRejectedValueOnce(error)
-    const request = makeRequest()
+  test("Should return 500 if surveyId throws", async () => {
+    const { sut, loadSurveyByIdStub } = makeSut();
+    const error = new Error();
+    jest.spyOn(loadSurveyByIdStub, "loadById").mockRejectedValueOnce(error);
+    const request = mockRequest();
 
-    const response = await sut.handle(request)
-    expect(response).toStrictEqual(serverError(error))
-  })
+    const response = await sut.handle(request);
+    expect(response).toStrictEqual(serverError(error));
+  });
 
-  test('Should return 403 if an invalid answer is provided', async () => {
-    const { sut } = makeSut()
-    const request = makeRequest()
+  test("Should return 403 if an invalid answer is provided", async () => {
+    const { sut } = makeSut();
+    const request = mockRequest();
     request.body = {
-      answer: 'wrong-answer'
-    }
+      answer: "wrong-answer",
+    };
 
-    const response = await sut.handle(request)
-    expect(response).toStrictEqual(forbidden(new InvalidParamError('answer')))
-  })
+    const response = await sut.handle(request);
+    expect(response).toStrictEqual(forbidden(new InvalidParamError("answer")));
+  });
 
-  test('Should call SaveSurveyResult with correct args', async () => {
-    const { sut, saveSurveyResultStub } = makeSut()
-    const saveSpy = jest.spyOn(saveSurveyResultStub, 'save')
-    const request = makeRequest()
-    await sut.handle(request)
+  test("Should call SaveSurveyResult with correct args", async () => {
+    const { sut, saveSurveyResultStub } = makeSut();
+    const saveSpy = jest.spyOn(saveSurveyResultStub, "save");
+    const request = mockRequest();
+    await sut.handle(request);
 
     expect(saveSpy).toBeCalledWith({
       accountId: request.accountId,
       surveyId: request.params.surveyId,
       answer: request.body.answer,
-      date: new Date()
-    })
-  })
+      date: new Date(),
+    });
+  });
 
-  test('Should return 500 if SaveSurveyResult throws', async () => {
-    const { sut, saveSurveyResultStub } = makeSut()
-    const error = new Error()
-    jest.spyOn(saveSurveyResultStub, 'save').mockRejectedValueOnce(error)
-    const request = makeRequest()
+  test("Should return 500 if SaveSurveyResult throws", async () => {
+    const { sut, saveSurveyResultStub } = makeSut();
+    const error = new Error();
+    jest.spyOn(saveSurveyResultStub, "save").mockRejectedValueOnce(error);
+    const request = mockRequest();
 
-    const response = await sut.handle(request)
-    expect(response).toStrictEqual(serverError(error))
-  })
+    const response = await sut.handle(request);
+    expect(response).toStrictEqual(serverError(error));
+  });
 
-  test('Should return 200 on success', async () => {
-    const { sut} = makeSut()
-    const request = makeRequest()
+  test("Should return 200 on success", async () => {
+    const { sut } = makeSut();
+    const request = mockRequest();
 
-    const response = await sut.handle(request)
-    expect(response).toStrictEqual(ok(mockSurveyResult()))
-  })
-})
+    const response = await sut.handle(request);
+    expect(response).toStrictEqual(ok(mockSurveyResult()));
+  });
+});
